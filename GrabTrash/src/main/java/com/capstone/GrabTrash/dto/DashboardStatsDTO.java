cfd08998bb9917a571d12e88daa00b81a6a4bc73
Anzyyy <foxtrot091503@gmail.com>
2025-05-13 21:48:19 +0800

    Updated DashboardStatsDTO

--- GrabTrash/src/main/java/com/capstone/GrabTrash/dto/DashboardStatsDTO.java
+++ GrabTrash/src/main/java/com/capstone/GrabTrash/dto/DashboardStatsDTO.java
@@ -1,21 +1,95 @@
 package com.capstone.GrabTrash.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 /**
  * Data Transfer Object for dashboard statistics
  * This class is used to send summarized data for the dashboard
  */
-@Data
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class DashboardStatsDTO {
     private int totalPickupTrashOrdered;
     private double totalRevenue;
     private int totalCompletedOrders;
     private String message;
+    
+    // Default constructor
+    public DashboardStatsDTO() {
+    }
+    
+    // All-args constructor
+    public DashboardStatsDTO(int totalPickupTrashOrdered, double totalRevenue, int totalCompletedOrders, String message) {
+        this.totalPickupTrashOrdered = totalPickupTrashOrdered;
+        this.totalRevenue = totalRevenue;
+        this.totalCompletedOrders = totalCompletedOrders;
+        this.message = message;
+    }
+    
+    // Getters
+    public int getTotalPickupTrashOrdered() {
+        return totalPickupTrashOrdered;
+    }
+    
+    public double getTotalRevenue() {
+        return totalRevenue;
+    }
+    
+    public int getTotalCompletedOrders() {
+        return totalCompletedOrders;
+    }
+    
+    public String getMessage() {
+        return message;
+    }
+    
+    // Setters
+    public void setTotalPickupTrashOrdered(int totalPickupTrashOrdered) {
+        this.totalPickupTrashOrdered = totalPickupTrashOrdered;
+    }
+    
+    public void setTotalRevenue(double totalRevenue) {
+        this.totalRevenue = totalRevenue;
+    }
+    
+    public void setTotalCompletedOrders(int totalCompletedOrders) {
+        this.totalCompletedOrders = totalCompletedOrders;
+    }
+    
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
+    // Builder static class
+    public static class Builder {
+        private int totalPickupTrashOrdered;
+        private double totalRevenue;
+        private int totalCompletedOrders;
+        private String message;
+        
+        public Builder totalPickupTrashOrdered(int totalPickupTrashOrdered) {
+            this.totalPickupTrashOrdered = totalPickupTrashOrdered;
+            return this;
+        }
+        
+        public Builder totalRevenue(double totalRevenue) {
+            this.totalRevenue = totalRevenue;
+            return this;
+        }
+        
+        public Builder totalCompletedOrders(int totalCompletedOrders) {
+            this.totalCompletedOrders = totalCompletedOrders;
+            return this;
+        }
+        
+        public Builder message(String message) {
+            this.message = message;
+            return this;
+        }
+        
+        public DashboardStatsDTO build() {
+            return new DashboardStatsDTO(totalPickupTrashOrdered, totalRevenue, totalCompletedOrders, message);
+        }
+    }
+    
+    // Static builder factory method
+    public static Builder builder() {
+        return new Builder();
+    }
 } 
